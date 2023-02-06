4f0a7867f4edbc81a944baef1d08ad8de875961d
zach-gousseau <zgousseau@gmail.com>
2023-02-06 09:40:44 -0800

    ice experiment

--- graph_functions.py
+++ graph_functions.py
@@ -30,8 +30,18 @@ def create_graph_structure(graph_nodes, distances):
     edge_attrs = torch.tensor(edge_attrs, dtype=torch.float)
     return Data(edge_index=edge_index, edge_attr=edge_attrs)
 
+def create_blocks(M, N, B):
+    num_rows = -(M // -B)
+    num_cols = -(N // -B)
+    label = np.arange(num_rows * num_cols).reshape(num_rows, num_cols)
+    blocks = np.repeat(label, B, axis=0)
+    blocks = np.repeat(blocks, B, axis=1)
+    return blocks[:M, :N]
 
-def quadtree_decompose(img, padding=2, thresh=0.05):
+def is_power_of_two(n):
+    return (n != 0) and (n & (n-1) == 0)
+
+def quadtree_decompose(img, padding=2, thresh=0.05, max_size=8):
     """
     Decompose an image into a quadtree.
 
@@ -48,51 +58,68 @@ def quadtree_decompose(img, padding=2, thresh=0.05):
     Returns:
     np.ndarray: Array of shape (height, width) containing the labels for each pixel in the image.
     """
+
+    assert is_power_of_two(max_size)
+
     # get image dimensions
-    n, m = shape = img.shape
+    n, m = img.shape
     
     # initialize label array
-    labels = np.zeros_like(img).astype(int)
+    labels = np.ones((-(n // -max_size) * max_size, -(m // -max_size) * max_size)).astype(int)  #create_blocks(n, m, max_size)
+
+    shape = n_padded, m_padded = labels.shape
+
+    img = np.pad(img, ((0, n_padded-n), (0, m_padded-m)), mode='edge')
     
     # counter for current label
     global cur_label
     cur_label = 0
-    
+
     # function for recursive decomposition
     def decompose(x: int, y: int, size: int):
         global cur_label
+
         # check if the current cell is homogeneous (contains pixels of the same color)
         l, r, t, b = x, x+size+1, y, y+size+1
-        if size == 1:
-            labels[x, y] = cur_label
-            cur_label += 1
+        if size <= 1 and (x<m and y<n):
+            if (x<n and y<m):
+                labels[x, y] = cur_label
+                cur_label += 1
             return
-        is_homogeneous = (np.nanvar(
+        
+        is_homogeneous = (np.nanmax(
         img[max(0, l-padding): min(r+padding, shape[1]),
             max(0, t-padding): min(b+padding, shape[1])]
         ) < thresh) or (size==1)
         
         # if the cell is homogeneous, assign the same label to all its pixels
-        if is_homogeneous:
-            labels[x:x+size, y:y+size] = cur_label
-            cur_label += 1
+        if is_homogeneous and size < max_size:
+            # print(1, labels[x:x+size, y:y+size].shape)
+            if (x<n and y<m):
+                labels[x:x+size, y:y+size] = cur_label
+                cur_label += 1
         else:
             # otherwise, decompose the cell into four quadrants of equal size
             new_size = size // 2
+            # print(x, y, new_size)
             decompose(x, y, new_size)
             decompose(x + new_size, y, new_size)
             decompose(x, y + new_size, new_size)
             decompose(x + new_size, y + new_size, new_size)
     
     # start recursive decomposition from the top left corner of the image
-    decompose(0, 0, n)
-    
+    for i in range(n_padded // max_size): 
+        for j in range(m_padded // max_size):
+            # print(i*max_size, j*max_size)
+            decompose(i*max_size, j*max_size, max_size)
+
     # return the resulting label array
-    return labels
+    return labels[:n, :m]
+
 
 
 def get_adj(labels):
-    """Get the adjacency matrix for a given label matrix"""
+    """Get the adjacency matrix for a given label matrix (this could be more efficient)"""
     w, h = labels.shape
     adj_dict = {}
 
@@ -101,19 +128,24 @@ def get_adj(labels):
             if labels[i][j] not in adj_dict:
                 adj_dict[labels[i][j]] = []
 
-            neighbors = []
+            neighbors = set()
 
             if i != 0:
-                neighbors.append(labels[i-1][j])
+                neighbors.add(labels[i-1][j])
             if i != w-1:
-                neighbors.append(labels[i+1][j])
+                neighbors.add(labels[i+1][j])
             if j != 0:
-                neighbors.append(labels[i][j-1])
+                neighbors.add(labels[i][j-1])
             if j != h-1:
-                neighbors.append(labels[i][j+1])
+                neighbors.add(labels[i][j+1])
+
+            try:
+                neighbors.remove(labels[i][j])
+            except KeyError:
+                pass
 
             for neighbor in neighbors:
-                if (neighbor != labels[i][j]) & (neighbor not in adj_dict[labels[i][j]]):
+                if neighbor not in adj_dict[labels[i][j]]:
                     adj_dict[labels[i][j]].append(neighbor)
 
     return adj_dict
@@ -126,7 +158,14 @@ def dist_xy(node0, node1, xx, yy):
 
 def flatten(img, labels, num_features=4):
     graph_nodes = sorted(np.unique(labels))
-    map_pixel_to_graph = {i: labels.flatten('C')[i] for i in np.arange(img.shape[-2]*img.shape[-3])}
+
+    map_pixel_to_graph = {}
+    p = 0
+    for i in range(img.shape[-3]):
+        for j in range(img.shape[-2]):
+            map_pixel_to_graph[p] = labels[i, j]
+            p += 1
+
     map_graph_to_pixel = {n: [] for n in graph_nodes}
     for p, n in map_pixel_to_graph.items():
         map_graph_to_pixel[n].append(p)
@@ -138,20 +177,22 @@ def flatten(img, labels, num_features=4):
 
     img_flat = img.reshape(-1, img.shape[-2]*img.shape[-3], num_features, order='C')
     data = np.ndarray((img.shape[0], len(graph_nodes), num_features))
+
     for i, n in enumerate(graph_nodes):
         idx = mappings['n->p'][n]
         data[:, i] = np.mean(img_flat[:, idx], 1)
     return data, graph_nodes, mappings
 
 def unflatten(img_flat, graph_nodes, mappings, image_shape=(8, 8)):
-    img = np.ndarray((img_flat.shape[0], np.prod(image_shape)))
+    img = np.ndarray((img_flat.shape[0], np.prod(image_shape), img_flat.shape[-1]))
+    # print(graph_nodes)
     for n in graph_nodes:
         for p in mappings['n->p'][n]:
             img[:, p] = img_flat[:, n]
 
-    return img.reshape((img_flat.shape[0], *image_shape))
+    return img.reshape((img_flat.shape[0], *image_shape, img_flat.shape[-1]))
 
-def image_to_graph(img, num_features=4, thresh=0.05):
+def image_to_graph(img, num_features=4, thresh=0.05, max_grid_size=8):
     """
     Decomposes an image into a quadtree and then generates a graph representation of the image
     using quadtree decomposition. The graph nodes are the centroids of the quadtree cells and the
@@ -170,31 +211,46 @@ def image_to_graph(img, num_features=4, thresh=0.05):
         graph_nodes (list): List of graph nodes.
         adj_dict (dict): Dictionary of adjacencies for each graph node.
         mappings (dict): Dictionary mapping graph node labels to indices.
+
+    TODO: Add ability to choose which channel to use in the decomposition.
     """
 
     img0 = np.max(img[..., 0], 0)  # For multi-step inputs
-    labels = quadtree_decompose(img0, thresh=thresh)
+    image_shape = img0.shape
+
+    if np.any(np.isnan(img0)):
+        raise ValueError('NaNs in data!!')
+
+    labels = quadtree_decompose(img0, thresh=thresh, max_size=max_grid_size)
 
     data, graph_nodes, mappings = flatten(img, labels, num_features)
 
-    yy, xx = data[0, ..., 1], data[0, ..., 2]
+    if np.any(np.isnan(data)):
+        raise ValueError('NaNs in data!')
+
+    xx, yy = data[0, ..., 1]*image_shape[1], data[0, ..., 2]*image_shape[0]
     
     # Get sizes for each graph node
-    node_sizes = np.array([np.sum(labels == label) for label in graph_nodes]).astype(float)
+    # node_sizes = np.array([np.sum(labels == label) for label in graph_nodes]).astype(float)
+    node_sizes = np.unique(labels, return_counts=True)[1]
+
+    # Pseudo-normalize
+    node_sizes = node_sizes / ((max_grid_size/2)**2)
     node_sizes = np.tile(node_sizes, (img.shape[0], 1))
     data = np.concatenate([data, np.expand_dims(node_sizes, -1)], -1)
 
     adj_dict = get_adj(labels)
+    # print(list(np.array(graph_nodes[1:]) - np.array(graph_nodes[:-1])))
 
     # Get distances
     distances = {}
     for node in graph_nodes:
         distances[node] = {}
         for neighbor in adj_dict[node]:
-            distances[node][neighbor] = dist(node, neighbor, xx, yy)
+            distances[node][neighbor] = dist(node, neighbor, xx, yy) #/ max_grid_size *8
 
     # Normalize data to (0, 1)
-    data = data / np.max(data, 1)[:, None, :]
+    # data = data / np.max(data, 1)[:, None, :]
 
     out = dict(
         labels=labels,
--- utils.py
+++ utils.py
@@ -11,6 +11,48 @@ def get_n_params(model):
     return pp
 
 
+def add_positional_encoding(x, num_timesteps):
+    image_shape = x[0].shape[1:]
+
+    # Position encoding
+    ii = np.tile(np.array(range(image_shape[1])), (image_shape[0], 1))
+    jj = np.tile(np.array(range(image_shape[0])), (image_shape[1], 1)).T
+
+    # Normalize
+    ii = ii / image_shape[1]
+    jj = jj / image_shape[0]
+
+    # ii = ii +0.1
+    # jj = jj +0.1
+
+    # print(jj)
+    # print('allo')
+
+    # ii = ii / image_shape[0]
+    # jj = jj / image_shape[0]
+
+    # print(jj)
+    # print('voila')
+
+    pos_encoding = np.moveaxis(np.array([[ii, jj]]*num_timesteps), 1, -1)
+
+    # print(pos_encoding)
+    # fds
+
+    # print(x.shape)
+    # print(np.array([pos_encoding]*len(x)).shape)
+    
+    # print(x.shape)
+    x = np.concatenate((x, np.array([pos_encoding]*len(x))), axis=-1)
+    # print('par ici')
+    # print(x[0, 0, :4, :4, 1])
+    # import matplotlib.pyplot as plt
+    # plt.imshow(x[0, 0, :, :, 0])
+    # plt.colorbar()
+    # plt.show()
+    # ds
+    return x
+
 
 def gen_x_y(arr, input_ts=1, batch_size=8, num_vars=4):
     i = 0
@@ -26,3 +68,8 @@ def gen_x_y(arr, input_ts=1, batch_size=8, num_vars=4):
             i += 1
         xs, ys = np.array(xs), np.array(ys)
         yield xs, ys[:, :, :1]
+        
+def normalize(arr):
+    min_ = np.min(arr, (0, 2, 3, 4))[:, None, None, None]
+    max_ = np.max(arr, (0, 2, 3, 4))[:, None, None, None]
+    return (arr - min_) / (max_ - min_)
