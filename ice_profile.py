fix input_timesteps specification & add truncated backprop

ice_profile.py
@@ -54,8 +54,8 @@ if __name__ == '__main__':
     binary_thresh = 0.15
 
     # Number of frames to read as input
-    input_timesteps = 5
-    output_timesteps= 30
+    input_timesteps = 30
+    output_timesteps= 90
 
     start = time.time()
 
@@ -71,8 +71,8 @@ if __name__ == '__main__':
     data_train = IceDataset(ds, training_years, month, input_timesteps, output_timesteps, x_vars, y_vars, train=True, y_binary_thresh=binary_thresh if binary else None)
     data_test = IceDataset(ds, [training_years[-1]+1], month, input_timesteps, output_timesteps, x_vars, y_vars, y_binary_thresh=binary_thresh if binary else None)
 
-    loader_profile = DataLoader(data_train, batch_size=1, sampler=torch.utils.data.SubsetRandomSampler(range(25)))
-    loader_test = DataLoader(data_train, batch_size=1, sampler=torch.utils.data.SubsetRandomSampler(range(10)))
+    loader_profile = DataLoader(data_train, batch_size=1, sampler=torch.utils.data.SubsetRandomSampler(range(5)))
+    loader_test = DataLoader(data_train, batch_size=1, sampler=torch.utils.data.SubsetRandomSampler(range(1)))
 
     # thresh = 0.15
     thresh = -np.inf
@@ -84,7 +84,7 @@ if __name__ == '__main__':
     model_kwargs = dict(
         hidden_size=8,
         dropout=0.1,
-        n_layers=4,
+        n_layers=1,
         transform_func=dist_from_05,
         dummy=False,
         convolution_type='TransformerConv'
@@ -96,6 +96,7 @@ if __name__ == '__main__':
         thresh=thresh,
         experiment_name=experiment_name,
         input_features=input_features,
+        input_timesteps=input_timesteps,
         output_timesteps=output_timesteps,
         transform_func=dist_from_05,
         binary=binary,
@@ -112,7 +113,7 @@ if __name__ == '__main__':
     import cProfile, pstats, io
     pr = cProfile.Profile()
     pr.enable()
-    model.train(loader_profile, loader_test, climatology, lr=lr, n_epochs=10, mask=mask)  # Train for 20 epochs
+    model.train(loader_profile, loader_test, climatology, lr=lr, n_epochs=1, mask=mask)  # Train for 20 epochs
     pr.disable()
     stats = pstats.Stats(pr).sort_stats('time')
     stats.print_stats(10)

mpnnlstm.py
@@ -111,6 +111,7 @@ class NextFramePredictorS2S(NextFramePredictor):
                  experiment_name='experiment', 
                  decompose=True, 
                  input_features=1,
+                 input_timesteps=3,
                  output_timesteps=3,
                  device=None,
                  transform_func=None,
@@ -128,11 +129,13 @@ class NextFramePredictorS2S(NextFramePredictor):
                  transform_func=transform_func,
                  condition=condition)
         
+        self.input_timesteps = input_timesteps
         self.output_timesteps = output_timesteps
         self.binary = binary
         
         self.model = Seq2Seq(
             input_features=input_features + 2,  # 3 (node_size)
+            input_timesteps=input_timesteps,
             output_timesteps=output_timesteps,
             thresh=thresh,
             device=device,
@@ -150,7 +153,8 @@ class NextFramePredictorS2S(NextFramePredictor):
         n_epochs=200,
         lr=0.01,
         lr_decay=0.95,
-        mask=None
+        mask=None,
+        truncated_backprop=45,
         ):
 
         image_shape = loader_train.dataset.image_shape
@@ -186,49 +190,81 @@ class NextFramePredictorS2S(NextFramePredictor):
                     skip = self.get_climatology_array(climatology, launch_date)
                 else:
                     skip = None
-
-                optimizer.zero_grad()
                 
-                # with amp.autocast():
-                y_hat, y_hat_mappings = self.model(x, y, skip, teacher_forcing_ratio=0, mask=mask)
-                y_hat = [unflatten(y_hat[i], y_hat_mappings[i], image_shape, mask) for i in range(self.output_timesteps)]
-                y_hat = torch.stack(y_hat, dim=0)
+                if truncated_backprop == 0:
+                    optimizer.zero_grad()
                 
-                loss = loss_func(y_hat[:, ~mask], y[:, ~mask])  
+                    # with amp.autocast():
+                    y_hat, y_hat_mappings = self.model(x, y, skip, teacher_forcing_ratio=0, mask=mask)
+                    
+                    y_hat = [unflatten(y_hat[i], y_hat_mappings[i], image_shape, mask) for i in range(self.output_timesteps)]
+                    y_hat = torch.stack(y_hat, dim=0)
+                    
+                    loss = loss_func(y_hat[:, ~mask], y[:, ~mask])  
 
 
-                # scaler.scale(loss).backward()
-                # scaler.step(optimizer)
-                # scaler.update()
+                    # scaler.scale(loss).backward()
+                    # scaler.step(optimizer)
+                    # scaler.update()
 
-                # with profiler.profile(enabled=True, use_cuda=True) as prof:
-                loss.backward()
+                    # with profiler.profile(enabled=True, use_cuda=True) as prof:
+                    loss.backward()
 
-                # print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
-                # prof.export_chrome_trace("profiling_results.json")
-                # quit()
+                    # print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
+                    # prof.export_chrome_trace("profiling_results.json")
+                    # quit()
 
-                optimizer.step()
+                    optimizer.step()
 
-                # decoder_params = [p for p in self.model.decoder.parameters()]
-                # decoder_grads = [p.grad.mean().cpu() for p in decoder_params if p.grad is not None]
-                # decoder_param_means = [p.mean().abs().detach().cpu() for p in decoder_params]
-                # writer.add_scalar("Grad/decoder/mean", np.mean(np.abs(decoder_grads)), epoch)
-                # writer.add_scalar("Param/decoder/mean", np.mean(decoder_param_means), epoch)
+                    # decoder_params = [p for p in self.model.decoder.parameters()]
+                    # decoder_grads = [p.grad.mean().cpu() for p in decoder_params if p.grad is not None]
+                    # decoder_param_means = [p.mean().abs().detach().cpu() for p in decoder_params]
+                    # writer.add_scalar("Grad/decoder/mean", np.mean(np.abs(decoder_grads)), epoch)
+                    # writer.add_scalar("Param/decoder/mean", np.mean(decoder_param_means), epoch)
 
-                # encoder_params = [p for p in self.model.encoder.parameters()]
-                # encoder_grads = [p.grad.mean().cpu() for p in encoder_params if p.grad is not None]
-                # encoder_param_means = [p.mean().abs().detach().cpu() for p in encoder_params]
-                # writer.add_scalar("Grad/encoder/mean", np.mean(np.abs(encoder_grads)), epoch)
-                # writer.add_scalar("Param/encoder/mean", np.mean(encoder_param_means), epoch)
+                    # encoder_params = [p for p in self.model.encoder.parameters()]
+                    # encoder_grads = [p.grad.mean().cpu() for p in encoder_params if p.grad is not None]
+                    # encoder_param_means = [p.mean().abs().detach().cpu() for p in encoder_params]
+                    # writer.add_scalar("Grad/encoder/mean", np.mean(np.abs(encoder_grads)), epoch)
+                    # writer.add_scalar("Param/encoder/mean", np.mean(encoder_param_means), epoch)
+
+                    del y_hat
+                    del y_hat_mappings
+
+                else:
+                    output_timestep = 0
+                    while output_timestep < self.output_timesteps:
+
+                        output_timestep = min(output_timestep + truncated_backprop, self.output_timesteps)
+
+                        optimizer.zero_grad()
+
+                        # Encoder
+                        self.model.process_inputs(x, mask=mask)
+
+                        # Decoder
+                        y_hat, y_hat_mappings = self.model.unroll_output(
+                            truncated_backprop,
+                            y,
+                            skip=skip,
+                            teacher_forcing_ratio=0,
+                            mask=mask,
+                            remesh_every=1
+                            )
+                    
+                        y_hat = [unflatten(y_hat[i], y_hat_mappings[i], image_shape, mask) for i in range(truncated_backprop)]
+                        y_hat = torch.stack(y_hat, dim=0)
+                        
+                        loss = loss_func(y_hat[:, ~mask], y[output_timestep-truncated_backprop:output_timestep, ~mask])  
+                        loss.backward()
+                        optimizer.step()
+
+                        del y_hat, y_hat_mappings
 
                 writer.add_scalar("Loss/train", loss.item(), epoch)
 
                 step += 1
                 running_loss += loss.item()
-
-                del y_hat
-                del y_hat_mappings
                 torch.cuda.empty_cache()
 
             running_loss_test = 0

seq2seq.py
@@ -212,8 +212,9 @@ class Seq2Seq(torch.nn.Module):
         self.graph = None
 
         self.device = device
-        
-    def forward(self, x, y=None, skip=None, teacher_forcing_ratio=0.5, mask=None, remesh_every=1):
+
+    def process_inputs(self, x, mask=None):
+
         num_samples, w, h, c = x.shape
         image_shape = (w, h)
 
@@ -234,10 +235,6 @@ class Seq2Seq(torch.nn.Module):
         self.graph.n_pixels_per_node = graph_structure['n_pixels_per_node']
         self.graph.image_shape = image_shape
         self.graph.pyg.to(self.device)
-        
-        # Lists to store decoder outputs
-        outputs = []
-        output_mappings = []
         
         # Encoder ------------------------------------------------------------------------------------------------------------
         self.graph.hidden, self.graph.cell = None, None
@@ -262,17 +259,22 @@ class Seq2Seq(torch.nn.Module):
                 else:
                     self.graph.hidden = hidden
                     self.graph.cell = cell
-        
-        # Decoder ------------------------------------------------------------------------------------------------------------
-        
+
         # Persistance
-        persistence = x[[-1]][:, :, :, [0]]
+        self.graph.persistence = x[[-1]][:, :, :, [0]]
         
         # First input to the decoder is the last input to the encoder 
         # self.graph.pyg.x = self.graph.pyg.x[-1, :, [0, -3, -2, -1]].unsqueeze(0)
         self.graph.pyg.x = self.graph.pyg.x[-1, :, [0, -2, -1]]  # node_size
 
-        for t in range(self.output_timesteps):
+
+    def unroll_output(self, unroll_steps, y, skip=None, teacher_forcing_ratio=0.5, mask=None, remesh_every=1):
+
+        # Lists to store decoder outputs
+        outputs = []
+        output_mappings = []
+
+        for t in range(unroll_steps):
             # print('Decoder step', t)
             # print("torch.cuda.memory_allocated: %fGB"%(torch.cuda.memory_allocated(0)/1024/1024/1024))
             # print("torch.cuda.memory_reserved: %fGB"%(torch.cuda.memory_reserved(0)/1024/1024/1024))
@@ -283,9 +285,9 @@ class Seq2Seq(torch.nn.Module):
             # print('memory use:', memoryUse, end='\r')
             
             if skip is not None:
-                skip_t = torch.cat([skip[t].unsqueeze(0), persistence], dim=-1)
+                skip_t = torch.cat([skip[t].unsqueeze(0), self.graph.persistence], dim=-1)
             else:
-                skip_t = persistence
+                skip_t = self.graph.persistence
 
             skip_t = flatten(skip_t, self.graph.mapping, self.graph.n_pixels_per_node, self.mask).squeeze(0)
 
@@ -316,6 +318,25 @@ class Seq2Seq(torch.nn.Module):
                 self.do_remesh(output, hidden, cell, mask, teacher_force=teacher_force, teacher_input=teacher_input)
             else:
                 self.update_without_remesh(output, hidden, cell, teacher_force=teacher_force, teacher_input=teacher_input)
+
+        return outputs, output_mappings
+        
+
+        
+    def forward(self, x, y=None, skip=None, teacher_forcing_ratio=0.5, mask=None, remesh_every=1):
+
+        # Encoder
+        self.process_inputs(x, mask=mask)
+
+        # Decoder
+        outputs, output_mappings = self.unroll_output(
+            self.output_timesteps,
+            y,
+            skip=skip,
+            teacher_forcing_ratio=teacher_forcing_ratio,
+            mask=mask,
+            remesh_every=remesh_every
+            )
             
         return outputs, output_mappings
 