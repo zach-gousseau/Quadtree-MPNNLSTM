1fdf71c734ae452af367cdbf1876e6c55a173466
Zach Gousseau <zgousseau@gmail.com>
2025-05-26 20:18:32 -0700

    Create ice_results_cnn.sh

---
+++ ice_results_cnn.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --account=def-ka3scott
+#SBATCH --mem=187G
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00-1:00            # time (DD-HH:MM)
+#SBATCH --output=/home/zgoussea/scratch/logs/ice_results_cnn_%A_%a.out
+
+module load StdEnv/2023
+module load gcc/12.3
+module load eccodes/2.31.0
+module load openmpi/4.1.5
+module load hdf5/1.14.2
+module load netcdf/4.9.2
+source /home/zgoussea/geospatial/bin/activate
+
+mpirun -np 1 python ice_results_cnn.py
+
